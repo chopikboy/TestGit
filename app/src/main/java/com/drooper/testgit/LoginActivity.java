9844705f883193445b9f2c55177a2423b37b0506
chopikboy <azathai@yandex.ru>
2015-01-18 21:40:25 +0300

    initial commit

---
+++ app/src/main/java/com/drooper/testgit/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.drooper.testgit;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class LoginActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
